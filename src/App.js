b4558004c177b90172004403c2077a7d29ff5e55
0tuedon <tuedonmofe@gmail.com>
2021-11-29 18:44:00 +0100

    Fixed the Unused Variables in the App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Tour from './components/Body/Tour';
 import Header from './components/Header/Header';
 
 function App() {
-  const url = 'https://course-api.com/react-tours-project';
-
   return (
     <div className="App">
       <Header />
