af2192c6f1c3558564f1d30ada7c00d8523bb925
Joshman <ejembijoshman@gmail.com>
2021-11-29 16:25:33 +0100

    create components and add style variables updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Body from './components/Body/Body';
+import Tour from './components/Body/Tour';
 import Header from './components/Header/Header';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Tour />
     </div>
   );
 }
--- src/components/Body/Tourcard.jsx
+++ src/components/Body/Tourcard.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import Button from '../ui/Button';
 
-const Ireland = () => {
+const Tourcard = () => {
     return (
         <div className="vienna">
-            Ireland Tour card
             <div className="image-display">
-
+                
             </div>
             <div className="tour-details">
+                <div className="top-title">
+                    <h3>Best of Title</h3>
+                    <span className="price">Amount</span>
+                </div>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit vitae error vero ullam. Explicabo ab earum, mollitia saepe ullam eveniet, minima voluptatibus ducimus modi fugiat expedita eligendi officiis, nihil quas?
                 </p>
+                <div className="bttn">
+                    <Button />
+                </div>
             </div>
         </div>
     );
 }
  
-export default Ireland;
+export default Tourcard;
