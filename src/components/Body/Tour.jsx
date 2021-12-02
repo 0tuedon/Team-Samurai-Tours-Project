9c05ea7a2415ad17583f1af3cafbc6d228f6ca4d
Ric <ric@Doom-Machine.local>
2021-12-02 09:55:43 +0100

    Added a refresh button that refreshes the page when there are no more tours to show

--- src/components/Body/Tour.jsx
+++ src/components/Body/Tour.jsx
@@ -1,66 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import './Tour.css'
+import React, { useState, useEffect } from 'react';
+import './Tour.css';
 import Tourcard from './Tourcard';
 
 const Tour = () => {
-    const [toursData, setToursData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const deleteTour = (name) => {
-        
-        const newToursData = toursData.filter((tour) => tour.name !== name)
-        setTimeout(setToursData(newToursData),2000)
-    }
+  const [toursData, setToursData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const deleteTour = (name) => {
+    const newToursData = toursData.filter(
+      (tour) => tour.name !== name
+    );
+    setTimeout(setToursData(newToursData), 2000);
+  };
 
-    useEffect(() => {
-        const url = "https://course-api.com/react-tours-project";
+  useEffect(() => {
+    const url =
+      'https://course-api.com/react-tours-project';
 
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const response = await fetch(url);
-                if(response.ok)
-                {
-                    console.log('fefe')
-                const json = await response.json();
-                setToursData(json);
-                console.log(json);
-                setLoading(false)
-                }
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        if (response.ok) {
+          console.log('fefe');
+          const json = await response.json();
+          setToursData(json);
+          console.log(json);
+          setLoading(false);
+        }
+      } catch (error) {
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            } catch (error){
-               
-                setLoading(false)
-            }
-            finally {
-                setLoading(false)
-            }
-        };
+    fetchData();
+  }, []);
 
-        fetchData();
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
 
-    }, []);
-
-
-    return (!loading ? (toursData.length === 0 ? <h1 className="no-tour">No Tour Saved 😥</h1> :
-        <div className="tour-body">
-            <h2>Our Tours</h2>
-            <div className="tour-holder">
-                {
-                    toursData.map((data) => {
-                        return (
-                            <Tourcard
-                                key={data.id}
-                                deleteTour={deleteTour}
-                                Data={data}
-                            />
-                        )
-                    })
-                }
-            </div>
-        </div>) : <h2 className="loading">Loading...</h2>
-
+  return !loading ? (
+    toursData.length === 0 ? (
+      <div className='no-tour'>
+        <h1>No More Tours Remaining</h1>
+        <button
+          style={{
+            background: '#008bff',
+            color: '#fff',
+            padding: '10px',
+            border: 'none',
+            borderRadius: '3px',
+            cursor: 'pointer',
+          }}
+          onClick={refreshPage}>
+          Refresh
+        </button>
+      </div>
+    ) : (
+      <div className='tour-body'>
+        <h2>Our Tours</h2>
+        <div className='tour-holder'>
+          {toursData.map((data) => {
+            return (
+              <Tourcard
+                key={data.id}
+                deleteTour={deleteTour}
+                Data={data}
+              />
+            );
+          })}
+        </div>
+      </div>
     )
-
-}
+  ) : (
+    <h2 className='loading'>Loading...</h2>
+  );
+};
 
 export default Tour;
